c826b4511617a850f890f3b75e468c6c649c9ade
eshawnjohal <46774276+eshawnjohal@users.noreply.github.com>
2019-01-16 23:00:28 -0800

    Update product.py

--- product.py
+++ product.py
@@ -4,15 +4,13 @@ class Product:
     def __init__(self, name, sku, department_name, cost, price, quantity):
         if quantity < 0 or cost < 0 or price < 0:
             raise ValueError
-        self.department = departments.Departments(department_name)
-        self.name = name
-        self.sku = sku
-        self.cost = cost
-        self.price = price
-        self.quantity = quantity
-
-    def __del__(self):
-        del self
+		else:
+        	self.department = departments.Departments(department_name)
+        	self.name = name
+        	self.sku = sku
+        	self.cost = cost
+        	self.price = price
+        	self.quantity = quantity
 
     def __str__(self):
         return f'''\nItem Name: {self.name}
